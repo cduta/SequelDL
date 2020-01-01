45e027fe566e59ca920b3ca4d186513acfd87acf
Christian Duta <christianmduta@gmail.com>
2020-01-01 10:19:36 +0100

    x1,y1 -> there_x,there_y

--- backend/sql/bresenham.sql
+++ backend/sql/bresenham.sql
@@ -1,6 +1,6 @@
 -- Apply Bresenham's line drawing algorithm to a line connecting l.here and l.there
 WITH RECURSIVE
-preparation(id, object_id, x0, y0, x1, y1, dx, dy, sx, sy) AS (
+preparation(id, object_id, x0, y0, there_x, there_y, dx, dy, sx, sy) AS (
   SELECT l.id, l.object_id, l.here_x, l.here_y, l.there_x, l.there_y, 
     ABS(l.here_x - l.there_x), 
     -ABS(l.here_y - l.there_y),
@@ -8,8 +8,8 @@ preparation(id, object_id, x0, y0, x1, y1, dx, dy, sx, sy) AS (
     CASE WHEN l.here_y < l.there_y THEN 1 ELSE -1 END 
   FROM   lines AS l 
 ),
-bresenham(id, object_id, x, y, error, x1, y1, dx, dy, sx, sy) AS (
-  SELECT p.id, p.object_id, p.x0, p.y0, p.dx+p.dy, p.x1, p.y1, p.dx, p.dy, p.sx, p.sy
+bresenham(id, object_id, x, y, error, there_x, there_y, dx, dy, sx, sy) AS (
+  SELECT p.id, p.object_id, p.x0, p.y0, p.dx+p.dy, p.there_x, p.there_y, p.dx, p.dy, p.sx, p.sy
   FROM   preparation AS p 
     UNION ALL 
   SELECT b.id, b.object_id,
@@ -26,11 +26,11 @@ bresenham(id, object_id, x, y, error, x1, y1, dx, dy, sx, sy) AS (
     WHEN 2*b.error >= b.dy               THEN b.error + b.dy
     WHEN 2*b.error <= b.dx               THEN b.error + b.dx 
     ELSE b.error                     
-  END, b.x1, b.y1, b.dx, b.dy, b.sx, b.sy
+  END, b.there_x, b.there_y, b.dx, b.dy, b.sx, b.sy
   FROM   bresenham AS b
-  WHERE  NOT (b.x = b.x1 AND b.y = b.y1)
+  WHERE  NOT (b.x = b.there_x AND b.y = b.there_y)
 )
-SELECT b.id, b.object_id, b.x, b.y, b.x1, b.y1, c.r, c.g, c.b, c.a
+SELECT b.id, b.object_id, b.x, b.y, b.there_x, b.there_y, c.r, c.g, c.b, c.a
 FROM   bresenham AS b, colors AS c 
 WHERE  b.object_id = c.object_id
 ORDER BY b.object_id;
