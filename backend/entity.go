b4a3030018c53f236adf46505872b5cf775cc3f6
Christian Duta <christianmduta@gmail.com>
2020-05-03 17:01:04 +0200

    Clean up rewrite of the collision detection

--- backend/entity.go
+++ backend/entity.go
@@ -8,23 +8,21 @@ func (handle *Handle) HasEntityPixelCollision(entityId int64, position Position)
   )
 
   row, err = handle.queryRow(`
-SELECT COALESCE((
-    SELECT en.id = ?
-    FROM   scenes          AS sc,
-           entities_scenes AS es,
-           entities        AS en,
-           hitboxes        AS hi  
-    WHERE  sc.id = es.scene_id 
-    AND    en.id = es.entity_id
-    AND    en.id = hi.entity_id
-    AND    ? BETWEEN MAX( sc.x + en.x + hi.relative_x - sc.scroll_x            , sc.x             ) 
-                 AND MIN( sc.x + en.x + hi.relative_x - sc.scroll_x + hi.width , sc.x + sc.width  ) 
-    AND    ? BETWEEN MAX( sc.y + en.y + hi.relative_y - sc.scroll_y            , sc.y             ) 
-                 AND MIN( sc.y + en.y + hi.relative_y - sc.scroll_y + hi.height, sc.y + sc.height )
-    ORDER BY en.level DESC, hi.level DESC
-    LIMIT 1
-  ), FALSE
-);`, entityId, position.X, position.Y)
+SELECT TOTAL(en.id = ?) AND TRUE
+FROM   scenes          AS sc,
+       entities_scenes AS es,
+       entities        AS en,
+       hitboxes        AS hi  
+WHERE  sc.id = es.scene_id 
+AND    en.id = es.entity_id
+AND    en.id = hi.entity_id
+AND    ? BETWEEN MAX( sc.x + en.x + hi.relative_x - sc.scroll_x            , sc.x             ) 
+             AND MIN( sc.x + en.x + hi.relative_x - sc.scroll_x + hi.width , sc.x + sc.width  ) 
+AND    ? BETWEEN MAX( sc.y + en.y + hi.relative_y - sc.scroll_y            , sc.y             ) 
+             AND MIN( sc.y + en.y + hi.relative_y - sc.scroll_y + hi.height, sc.y + sc.height )
+ORDER BY en.level DESC, hi.level DESC
+LIMIT 1;
+`, entityId, position.X, position.Y)
   if err != nil {
     return collision, err
   }
